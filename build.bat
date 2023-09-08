1ddb31b3e275802504e3f32fcd3b0d3c1011dd02
natsurainko <a-275@qq.com>
2023-09-08 11:44:54 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 cd .\Scripts
-dotnet build /p:Platform="Any CPU"
+dotnet build --configuration Release /p:Platform="Any CPU"
 cd ..\
-.\Scripts\bin\Debug\net7.0\LocalizerScript.exe .\Views .\
+.\Scripts\bin\Release\net7.0\LocalizerScript.exe .\Views .\
 pause
